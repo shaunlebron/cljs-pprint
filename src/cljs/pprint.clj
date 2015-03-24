0482f04e898557460f1ed19e7d2974e2076907b1
Jonathan Boston <bostonou@gmail.com>
2015-03-24 18:24:46 -0500

    parse-lb-options, pprint-logical-block, pll-mod-body, print-length-loop

--- src/cljs/pprint.clj
+++ src/cljs/pprint.clj
@@ -1,6 +1,7 @@
 
 (ns clojure.pprint
-  (:refer-clojure :exclude [deftype]))
+  (:refer-clojure :exclude [deftype])
+  (:require [clojure.walk :as walk]))
 
 
 ;; required the following changes:
@@ -38,4 +39,57 @@
          ~(vec fields)
          (~(symbol (str type-name ".")) ~(keyword name-str) ~@fields))
        (defn- ~(symbol (str name-str "?")) [x#] (= (:type-tag x#) ~(keyword name-str))))))
+
+(defn- parse-lb-options [opts body]
+  (loop [body body
+         acc []]
+    (if (opts (first body))
+      (recur (drop 2 body) (concat acc (take 2 body)))
+      [(apply hash-map acc) body])))
+
+(defmacro pprint-logical-block
+  "Execute the body as a pretty printing logical block with output to *out* which
+  must be a pretty printing writer. When used from pprint or cl-format, this can be
+  assumed.
+
+  This function is intended for use when writing custom dispatch functions.
+
+  Before the body, the caller can optionally specify options: :prefix, :per-line-prefix
+  and :suffix."
+  [& args]
+  (let [[options body] (parse-lb-options #{:prefix :per-line-prefix :suffix} args)]
+    `(do (if (#'clojure.pprint/level-exceeded)
+           (~'-write ~'*out* "#")
+           (do
+             (binding [#'clojure.pprint/*current-level* (inc #'clojure.pprint/*current-level*)
+                       #'clojure.pprint/*current-length* 0]
+               (#'clojure.pprint/start-block #'clojure.pprint/*out*
+                                             ~(:prefix options)
+                                             ~(:per-line-prefix options)
+                                             ~(:suffix options))
+               ~@body
+               (#'clojure.pprint/end-block #'clojure.pprint/*out*))))
+         nil)))
+
+(defn- pll-mod-body [var-sym body]
+  (letfn [(inner [form]
+                 (if (seq? form)
+                   (let [form (macroexpand form)]
+                     (condp = (first form)
+                       'loop* form
+                       'recur (concat `(recur (inc ~var-sym)) (rest form))
+                       (walk/walk inner identity form)))
+                   form))]
+    (walk/walk inner identity body)))
+
+(defmacro print-length-loop
+  "A version of loop that iterates at most *print-length* times. This is designed
+  for use in pretty-printer dispatch functions."
+  [bindings & body]
+  (let [count-var (gensym "length-count")
+        mod-body (pll-mod-body count-var body)]
+    `(loop ~(apply vector count-var 0 bindings)
+       (if (or (not #'cljs.core/*print-length*) (< ~count-var #'cljs.core/*print-length*))
+         (do ~@mod-body)
+         (~'-write #'clojure.pprint/*out* "...")))))
 
