7026eaadc528a0e073e7557fabba1094b82be21b
Jonathan Boston <bostonou@gmail.com>
2015-03-24 14:19:01 -0500

    Add macro for simple-tests

--- test/cljs/pprint.cljs
+++ test/cljs/pprint.cljs
@@ -1,3 +1,8 @@
 (ns clojure.test-clojure.pprint
-  (:require [cemerick.cljs.test :as t]))
+  (:require
+    [cemerick.cljs.test :as t]
+    [clojure.pprint :refer [pprint]])
+  (:require-macros
+    [cemerick.cljs.test :refer [deftest is]]
+    [pprint-test :refer [simple-tests]]))
 
