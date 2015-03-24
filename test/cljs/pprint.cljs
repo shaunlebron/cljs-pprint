53177b2ca5a0fc096a65b52a68ffc31f06e27807
Jonathan Boston <bostonou@gmail.com>
2015-03-24 14:19:16 -0500

    Add print length tests

--- test/cljs/pprint.cljs
+++ test/cljs/pprint.cljs
@@ -6,3 +6,49 @@
     [cemerick.cljs.test :refer [deftest is]]
     [pprint-test :refer [simple-tests]]))
 
+(simple-tests print-length-tests
+  (binding [*print-length* 1] (with-out-str (pprint '(a b c d e f))))
+  "(a ...)\n"
+  (binding [*print-length* 2] (with-out-str (pprint '(a b c d e f))))
+  "(a b ...)\n"
+  (binding [*print-length* 6] (with-out-str (pprint '(a b c d e f))))
+  "(a b c d e f)\n"
+  (binding [*print-length* 8] (with-out-str (pprint '(a b c d e f))))
+  "(a b c d e f)\n"
+
+  (binding [*print-length* 1] (with-out-str (pprint [1 2 3 4 5 6])))
+  "[1 ...]\n"
+  (binding [*print-length* 2] (with-out-str (pprint [1 2 3 4 5 6])))
+  "[1 2 ...]\n"
+  (binding [*print-length* 6] (with-out-str (pprint [1 2 3 4 5 6])))
+  "[1 2 3 4 5 6]\n"
+  (binding [*print-length* 8] (with-out-str (pprint [1 2 3 4 5 6])))
+  "[1 2 3 4 5 6]\n"
+
+  (binding [*print-length* 1] (with-out-str (pprint (sorted-set 1 2 3 4 5 6))))
+  "#{1 ...}\n"
+  (binding [*print-length* 2] (with-out-str (pprint (sorted-set 1 2 3 4 5 6))))
+  "#{1 2 ...}\n"
+  (binding [*print-length* 6] (with-out-str (pprint (sorted-set 1 2 3 4 5 6))))
+  "#{1 2 3 4 5 6}\n"
+  (binding [*print-length* 8] (with-out-str (pprint (sorted-set 1 2 3 4 5 6))))
+  "#{1 2 3 4 5 6}\n"
+
+  (binding [*print-length* 1] (with-out-str (pprint (sorted-map 1 2, 3 4, 5 6, 7 8, 9 10, 11 12))))
+  "{1 2, ...}\n"
+  (binding [*print-length* 2] (with-out-str (pprint (sorted-map 1 2, 3 4, 5 6, 7 8, 9 10, 11 12))))
+  "{1 2, 3 4, ...}\n"
+  (binding [*print-length* 6] (with-out-str (pprint (sorted-map 1 2, 3 4, 5 6, 7 8, 9 10, 11 12))))
+  "{1 2, 3 4, 5 6, 7 8, 9 10, 11 12}\n"
+  (binding [*print-length* 8] (with-out-str (pprint (sorted-map 1 2, 3 4, 5 6, 7 8, 9 10, 11 12))))
+  "{1 2, 3 4, 5 6, 7 8, 9 10, 11 12}\n"
+
+  (binding [*print-length* 1] (with-out-str (pprint (int-array [1 2 3 4 5 6]))))
+  "[1, ...]\n"
+  (binding [*print-length* 2] (with-out-str (pprint (int-array [1 2 3 4 5 6]))))
+  "[1, 2, ...]\n"
+  (binding [*print-length* 6] (with-out-str (pprint (int-array [1 2 3 4 5 6]))))
+  "[1, 2, 3, 4, 5, 6]\n"
+  (binding [*print-length* 8] (with-out-str (pprint (int-array [1 2 3 4 5 6]))))
+  "[1, 2, 3, 4, 5, 6]\n"
+)
