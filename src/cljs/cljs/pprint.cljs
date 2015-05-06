b6e1d1db9578e8edd58ae22fdf969c70b18c3ccc
Jonathan Boston <bostonou@gmail.com>
2015-05-05 19:00:58 -0500

    Add pprint-tab

--- src/cljs/cljs/pprint.cljs
+++ src/cljs/cljs/pprint.cljs
@@ -863,6 +863,24 @@ Output is sent to *out* which must be a pretty printing writer."
   (check-enumerated-arg relative-to #{:block :current})
   (indent *out* relative-to n))
 
+;; TODO a real implementation for pprint-tab
+(defn pprint-tab
+  "Tab at this point in the pretty printing stream. kind specifies whether the tab
+is :line, :section, :line-relative, or :section-relative.
+
+Colnum and colinc specify the target column and the increment to move the target
+forward if the output is already past the original target.
+
+This function is intended for use when writing custom dispatch functions.
+
+Output is sent to *out* which must be a pretty printing writer.
+
+THIS FUNCTION IS NOT YET IMPLEMENTED."
+  {:added "1.2"}
+  [kind colnum colinc]
+  (check-enumerated-arg kind #{:line :section :line-relative :section-relative})
+  (throw (js/Error. "pprint-tab is not yet implemented")))
+
 ;;======================================================================
 ;; cl_format.clj
 ;;======================================================================
